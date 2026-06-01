05ef3e9ce5869eabf08a678059deea9c6cff0c76
C0DREA <adrianslr030@gmail.com>
2026-06-01 20:42:25 +0300

    small fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Nexus Wallet',
+  base: '/Nexus_Wallet/',
   test: {
     environment: 'jsdom',
     globals: true,
